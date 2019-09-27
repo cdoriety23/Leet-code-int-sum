86e972af964fa06b924090f88ca5549078d67703
Chris D <cdmt@Chriss-MacBook-Pro.local>
2019-09-26 19:38:45 -0500

    Replacing solution file

---
+++ Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+		int one=0,two=0;
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			for(int j=0;j<nums.length;j++)
+			{
+				if((nums[i]+nums[j]==target) && (i != j))
+						{
+							one=i;
+							two=j;
+						}
+			}
+		}
+
+    
+  int[] index = new int[2];
+    index[1] =one;
+    index[0] =two;
+    return index;
+    }
+}   
