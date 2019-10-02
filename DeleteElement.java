0f814db736d9bd4badf037bc6f6bc4e54773e232
cdoriety23 <49670599+cdoriety23@users.noreply.github.com>
2019-10-01 20:13:53 -0500

    Add files via upload

---
+++ DeleteElement.java
@@ -0,0 +1,33 @@
+public class DeleteElement {
+
+	int number=0;
+	int [] arr = {};
+	
+	public DeleteElement(int[]nums,int number)
+	{
+		this.number = number;
+		arr= nums;
+	}
+	
+	
+		 public int removeElement() 
+		 {
+			    
+				
+		        int  i=0;
+				for (int j:arr)
+				{
+					if(j != number)
+					{
+						arr[i]= j;
+						i++;
+					}
+		          
+				}
+			return i;
+		}
+		
+		
+	}
+	
+
