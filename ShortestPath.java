ca12fa702c4d0135721c2a5b22f273c4ffe46b66
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-10-21 18:20:16 -0500

    UniquePath problem

--- ShortestPath.java
+++ ShortestPath.java
@@ -2,21 +2,34 @@ public class ShortestPath {
 	
 	public static void main(String args[])
 	{
-		System.out.println(uniquePaths(7,3));
+		System.out.println(uniquePaths(7,3));// 28
 	
 	
 	
 	}
 	
-		public static int uniquePaths(int m, int n) {
-		int[][] dp=new int[m][n];
+	public static int uniquePaths(int m, int n)
+	
+	{
+		int[][] sp=new int[m][n];
+		{
 		if (m<=1 || n<=1) return 1;
-		for (int i=0;i<m;i++){
-		for (int j=0;j<n;j++){
-		if (i == 0 || j == 0){dp[i][j]=1;}
-		else{dp[i][j]=dp[i-1][j]+dp[i][j-1];}
 		}
+		
+		for (int i=0;i<m;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+			if (i == 0 || j == 0)
+				{
+				sp[i][j]=1;
+				}
+			else
+				{
+				sp[i][j]=sp[i-1][j]+sp[i][j-1];
+				}
+			}
 		}
-		return dp[m-1][n-1];
-		}
-		}
+		return sp[m-1][n-1];
+	}
+}
