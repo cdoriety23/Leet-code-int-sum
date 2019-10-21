de55e6205f26920573c98622c7da313cc0ee4153
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-10-21 17:09:02 -0500

    ShortPath problem
    This problem is where a robot reaches finish while taking shortest path

---
+++ ShortestPath.java
@@ -0,0 +1,22 @@
+public class ShortestPath {
+	
+	public static void main(String args[])
+	{
+		System.out.println(uniquePaths(7,3));
+	
+	
+	
+	}
+	
+		public static int uniquePaths(int m, int n) {
+		int[][] dp=new int[m][n];
+		if (m<=1 || n<=1) return 1;
+		for (int i=0;i<m;i++){
+		for (int j=0;j<n;j++){
+		if (i == 0 || j == 0){dp[i][j]=1;}
+		else{dp[i][j]=dp[i-1][j]+dp[i][j-1];}
+		}
+		}
+		return dp[m-1][n-1];
+		}
+		}
