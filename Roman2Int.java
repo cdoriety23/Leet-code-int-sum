d8037e02635576ad63bfe6dc611f1f35109d550a
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-10-22 12:35:53 -0500

    Roman numeral to Int problem

--- Roman2Int.java
+++ Roman2Int.java
@@ -24,8 +24,8 @@ public class Roman2Int {
 		value.put('M', 1000);
 		
 	
-		int val1=0,val2=0;
-	int sum=0;
+		int val1=0,val2=0;int sum=0;
+
 		for(int i=0; i<s.length(); i++)
 		{
 			val1= value.get(s.charAt(i));
