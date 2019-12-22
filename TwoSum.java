1b1c717ff1ce6fc8035f6bfc7f6c46973504bc79
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-12-22 11:38:16 -0600

    TwoSumOptimized  Using HashMap

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+
+    public static void main(String args[]) {
+        int a [] =twoSum( new int[] {1,3,4,5,6,7,12},12);
+        System.out.println(Arrays.toString(a));
+
+    }
+
+    public static int[] twoSum(int [] num, int target) {
+        HashMap<Integer, Integer> sum = new HashMap<Integer,Integer>();
+
+        for(int i= 0; i<num.length; i++)
+        {
+            int addNum = target-num[i];
+            if(sum.containsKey(addNum))
+            {
+                return new int[] {sum.get(addNum), i};
+            }
+                sum.put(num[i],i);
+
+        }return null;
+
+    }
+}
