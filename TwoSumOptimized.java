e23f545478bb71ee02c8ba7dd378329b77f2e1db
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-12-22 11:39:33 -0600

    TwoSum updated using hashmap

--- TwoSumOptimized.java
+++ TwoSumOptimized.java
@@ -5,7 +5,7 @@ public class TwoSumOptimized {
 
 
     public static void main(String args[]) {
-        int a [] =twoSum( new int[] {1,3,4,5,6,7,12},12);
+        int a [] =twoSum( new int[] {1,3,4,5,6,7,12,20},12);
         System.out.println(Arrays.toString(a));
 
     }
