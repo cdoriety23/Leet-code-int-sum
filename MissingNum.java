26831f595c9aa6fd6bc9ec203531814157c86a45
Chris D <49670599+cdoriety23@users.noreply.github.com>
2019-12-27 16:20:13 -0600

    Missing Number

--- MissingNum.java
+++ MissingNum.java
@@ -2,7 +2,7 @@ public class MissingNum {
 
     public static void main(String args[])
     {
-            int [] a= new int[] {3,0,1};
+            int [] a= new int[] {3,0,1};// return 2
             System.out.println(missingNum(a));
     }
 
